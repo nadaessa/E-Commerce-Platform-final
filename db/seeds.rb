428f61be91417fa5579ba794b5438e8fba64603c
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-05-02 11:14:22 +0200

    add category and brand seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+5.times do
+    Brand.create([{
+        name: Faker::Appliance.brand
+    }])
+    end
+
+6.times do
+    Category.create([{
+        name: Faker::Commerce.department(1, true)
+    }])
+    end
