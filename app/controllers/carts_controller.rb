a00f3197c4c34c621ddcbdc9602145b3b000cb16
rawdasherif <rawdasherif119@gmail.com>
2019-05-03 04:07:31 +0200

    render data in cart page and total price

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,22 @@
 class CartsController < InheritedResources::Base
 
-  private
-
-    def cart_params
-      params.require(:cart).permit(:user_id)
+    def index
+      # params.require(:cart).permit(:user_id)
+      # params.require(:cart_item).permit(:quantity, :cart_id, :product_id)
+      @user =current_user.id
+      @cartId=Cart.select(:id).where(user_id: @user).last.id
+      @cartItems=CartItem.select(:quantity,:product_id).where(cart_id: @cartId)
+      
     end
+
+    private
+    def cart_params
+      params.require(:cart).permit(:user_id, :id)
+    end
+    def cart_item_params
+      params.require(:cart_item).permit(:quantity, :cart_id, :product_id)
+    end
+
+
 
 end
