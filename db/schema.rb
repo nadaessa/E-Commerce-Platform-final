3849faf88374e8eab74d3d868327f69de7498415
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-05-05 10:44:25 +0200

    add image in create product & drop image column from products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_080358) do
+ActiveRecord::Schema.define(version: 2019_05_05_083931) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "namespace"
@@ -124,7 +124,6 @@ ActiveRecord::Schema.define(version: 2019_05_05_080358) do
     t.text "description"
     t.float "price"
     t.integer "quantity"
-    t.string "image"
     t.bigint "store_id"
     t.bigint "category_id"
     t.bigint "brand_id"
