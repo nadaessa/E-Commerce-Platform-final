d1599e186523d0ed5e6481e01800b349d424f4fa
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-05-06 13:16:08 +0200

    add to cart function in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,7 +21,55 @@ class HomeController < ApplicationController
     end
      
     
+
+    # def addItem
+    #   @user = current_user.id
+    #   @cartId = Cart.select(:id).where(user_id:@user).last.id
+    #   @productId =params[:id]
+    #   @product_quantity= Product.select(:quantity).where(id:@productId).last.quantity
+    #   if @product_quantity > 0
+    #   CartItem.create(quantity:1, cart_id:@cartId , product_id:@productId )
+    #   flash[:success] = "added succesfully" 
+    #   else
+
+    #     flash[:alert] = "This product #{@productId}is not available" 
+    #   end
+    #   # @cartItem.save
+    #   # render plain: @cartId.inspect
+    #   redirect_to '/'
+      
+    # end
     
+  
+      def addItem
+          @user = current_user.id
+          @cartId = Cart.select(:id).where(user_id:@user).last.id
+          @productId =params[:id]
+          @product_quantity= Product.select(:quantity).where(id:@productId).last.quantity
+          if @product_quantity > 0
+              @ArrayProductId=CartItem.select(:product_id).where(cart_id: @cartId).all
+              @ArrayProductId.each do |product|
+                  if (product != @productId)
+                  CartItem.create(quantity:1, cart_id:@cartId , product_id:@productId )
+                  flash[:success] = "added succesfully"
+   
+                  elsif (product == @productId)
+                    flash[:alert] = "increase Q"
+                  end 
+                  # logger.debug @ArrayProductId.inspect
+                  # render plain: @ArrayProductId.inspect  
+              end    
+              
+          else
+
+            flash[:alert] = "This product #{@productId}is not available" 
+          end
+      
+          redirect_to '/'
+      
+    end
+
+
     private
 
     def product_params
