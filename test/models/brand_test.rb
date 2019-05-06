6a236fa10e9385c20e624e9800c4c19312206576
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-05-06 15:34:43 +0200

    add model test for brands

--- test/models/brand_test.rb
+++ test/models/brand_test.rb
@@ -4,4 +4,21 @@ class BrandTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  test "should not save brand without name" do
+    brand = Brand.new
+    assert_not brand.save
+  end
+
+
+  # test "should save brand with name" do
+  #   brand = Brand.new
+  #   assert brand.save
+  # end  
+  
+  test "should save brand with name" do
+    brand = Brand.new(name: "test" )
+    assert brand.save
+  end 
 end
