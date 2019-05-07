fe5ac25410612bef5db26ac01e44751e614a2f1d
rawdasherif <rawdasherif119@gmail.com>
2019-05-07 23:51:54 +0200

    buyer see order lists in orders views

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,28 +1,5 @@
 class OrderItemsController < InheritedResources::Base
-  def index
-    #get cart id for logged in user
-    @user=current_user.id
-    @@cartId=Cart.select(:id).where(user_id: @user).last.id
-    #get orders for this cart_id
-    @orders=Order.where(cart_id: @@cartId)
-    #loop in each order to get items in each one
-    @orders.each do|order|
-      @check=1
-      @order_item=OrderItem.select(:status).where(order_id: order.id)
-      @first_item=@order_item.first
-      @order_item.each do|item|
-        if @first_item.status!=item.status
-          @check=0
-          break  
-        end
-      end
-      if @check ==1
-        @status=@first_item.status
-        Order.where(id: order.id).update_all(order_status:@status)
-        break
-      end
-    end
-  end
+
 
   private
 
