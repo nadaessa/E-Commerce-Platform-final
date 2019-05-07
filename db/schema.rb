dc4b2107c6c6ea55bab8222eea872e4ba52ed356
nadaessa <nessam378@gmail.com>
2019-05-07 17:28:57 +0200

    Edit in seller index

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_05_07_135835) do
 
-  create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -59,13 +59,13 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "cart_items", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "cart_id"
     t.bigint "product_id"
@@ -75,33 +75,33 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["product_id"], name: "index_cart_items_on_product_id"
   end
 
-  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "cities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "cities", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.integer "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "countries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "countries", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "coupones", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "coupones", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "coupone_type"
     t.integer "value"
     t.integer "expiration_type"
@@ -113,7 +113,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "order_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "order_items", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.bigint "order_id"
     t.bigint "product_id"
     t.integer "quantity"
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
-  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.bigint "cart_id"
     t.integer "order_status"
     t.string "coupone_code"
@@ -137,7 +137,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["cart_id"], name: "index_orders_on_cart_id"
   end
 
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.float "price"
@@ -154,7 +154,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
-  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.integer "rating"
     t.text "comment"
     t.datetime "created_at", null: false
@@ -163,7 +163,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.integer "product_id"
   end
 
-  create_table "stores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "stores", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.text "summary"
     t.bigint "user_id"
@@ -172,7 +172,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["user_id"], name: "index_stores_on_user_id"
   end
 
-  create_table "user_coupones", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "user_coupones", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.bigint "coupone_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
@@ -181,7 +181,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_135835) do
     t.index ["user_id"], name: "index_user_coupones_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,42 +10,42 @@
 
  require 'csv'
 
- puts "Importing countries..."
- CSV.foreach(Rails.root.join("countries.csv"), headers: true) do |row|
-   Country.create! do |country|
-     country.id = row[0]
-     country.name = row[1]
-   end
- end
+#  puts "Importing countries..."
+#  CSV.foreach(Rails.root.join("countries.csv"), headers: true) do |row|
+#    Country.create! do |country|
+#      country.id = row[0]
+#      country.name = row[1]
+#    end
+#  end
  
- puts "Importing states..."
- CSV.foreach(Rails.root.join("cities.csv"), headers: true) do |row|
-   City.create! do |city|
-     city.name = row[0]
-     city.country_id = row[2]
-   end
- end
+#  puts "Importing states..."
+#  CSV.foreach(Rails.root.join("cities.csv"), headers: true) do |row|
+#    City.create! do |city|
+#      city.name = row[0]
+#      city.country_id = row[2]
+#    end
+#  end
  
 
-#5.times do
-    #Brand.create([{
-   #     name: Faker::Appliance.brand
-   # }])
-   # end
+5.times do
+    Brand.create([{
+       name: Faker::Appliance.brand
+    }])
+    end
 
-# 6.times do
-#     Category.create([{
-#         name: Faker::Commerce.department(1, true)
-#     }])
-#     end
+ 6.times do
+     Category.create([{
+         name: Faker::Commerce.department(1, true)
+     }])
+     end
 
-# 6.times do
-#     Store.create([{
-#         name: Faker::Commerce.department,
-#         summary: Faker::Lorem.sentence,
-#         user_id: 1
-#     }])
-#     end
+ 6.times do
+     Store.create([{
+         name: Faker::Commerce.department,
+         summary: Faker::Lorem.sentence,
+         user_id: 1
+     }])
+     end
 
 
 # 5.times do
